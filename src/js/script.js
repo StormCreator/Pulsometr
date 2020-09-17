03f330e61cd8b7f91924edaf4fd3e15c25c90484
MarginToppson <insetml@gmail.com>
2020-09-17 14:59:26 +0300

    adding slider Section

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,11 @@
-
-
 window.addEventListener('DOMContentLoaded', function() {
+    $('.slider__wrapper').slick({
+        autoplay: true,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider/prev-arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/next-arrow.png"></button>',
+    });
+    console.log('dasdae');
 });
 
 
